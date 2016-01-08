c62a07a798f855b96802bc70e7d2d66a128032fa
TheCrazyT <tobias15556@gmx.de>
2016-01-08 19:40:30 +0100

    Added julia script for later research. Maximum curve of 2^x seems to work.(instead of max curve of number itself) Maps are still pretty big(probably its never possible to test beyond 2^20) ... but there might be a better way.

---
+++ julia/README.txt
@@ -0,0 +1,13 @@
+some scripts for julia, see:
+http://julialang.org/
+for a program to execute them.
+
+run script with:
+
+julia script.jl
+
+or:
+
+include("path/to/script.jl")
+
+inside julia.(replace "script.jl" with the script name)
---
+++ julia/factorization_example.jl
@@ -0,0 +1,47 @@
+begin
+	SIZE=16
+	A = reshape([0:SIZE*SIZE-1] % SIZE + 1,SIZE,SIZE)
+	function F(x,y)
+			A[y,x]
+	end
+	B = [F(x,y) for x=[1:SIZE],y=[1:SIZE]]
+	
+	C = Array(Array,6)
+	C[1] = ((A.*B)&(2^0)).==2^0
+	C[2] = ((A.*B)&(2^1)).==2^1
+	C[3] = ((A.*B)&(2^2)).==2^2
+	C[4] = ((A.*B)&(2^3)).==2^3
+	C[5] = ((A.*B)&(2^4)).==2^4
+	C[6] = ((A.*B)&(2^5)).==2^5
+	
+	K = Array(Array,6)
+	K[1] = (A.*B).<=2^1
+	K[2] = (A.*B).<=2^2
+	K[3] = (A.*B).<=2^3
+	K[4] = (A.*B).<=2^4
+	K[5] = (A.*B).<=2^5
+	K[6] = (A.*B).<=2^6
+	
+	function custom_factorize(p)
+		L = 1
+		W = trues(SIZE,SIZE)
+		while 2^L <= SIZE
+			W = K[L]
+			L += 1
+		end
+
+		N = [2^x for x=[0:L]]
+		I = 0
+		for s in N
+			I += 1
+			if (p & s) != 0
+				W &= C[I]
+			else
+				W &= !C[I]
+			end
+		end
+		#*1 for smaller output
+		return W*1
+	end
+	custom_factorize(12)
+end
